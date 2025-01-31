3a28e5144f14d73c7782fe17c104ce9fc72bbf56
Yanis Cousseau <yanis.cousseau@eleves.ec-nantes.fr>
2025-01-31 10:42:58 +0100

    partie 1 et 2

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "point.hpp"
+using namespace std;
+
+int main(){
+    Point point1 =Point();
+    Point point2 =Point(5,2);
+    Point point3 =Point(point2);
+
+    double ord = point1.getOrdonnee();
+    double absc = point1.getAbscisse();
+
+    cout<<"Point3 : Ordonnée "<<point3.getOrdonnee()<<" Abscisse "<<point3.getAbscisse()<<endl;
+    cout<<"Point2 : Ordonnée "<<point2.getOrdonnee()<<" Abscisse "<<point2.getAbscisse()<<endl;
+    cout<<"Point1 : Ordonnée "<<ord<<" Abscisse "<<absc<<endl;
+
+    point1.translater(point2);
+    point2.translater(10, 10);
+
+    cout<<"Point1 : Ordonnée "<<point1.getOrdonnee()<<" Abscisse "<<point1.getAbscisse()<<endl;
+    cout<<"Point2 : Ordonnée "<<point2.getOrdonnee()<<" Abscisse "<<point2.getAbscisse()<<endl;
+
+    point3.setOrdonnee(20);
+    point3.setAbscisse(20);
+
+    cout<<"Point3 : Ordonnée "<<point3.getOrdonnee()<<" Abscisse "<<point3.getAbscisse()<<endl;
+    cout << point3<<endl;
+    point1+= point2;
+    cout << point1;
+    return 0;
+}
---
+++ point.hpp
@@ -0,0 +1,23 @@
+#include <ostream>
+class Point{
+private:
+    double ordonnees;
+    double abscisse;
+public :
+    void translater(Point const &newpoint);
+    void translater(double reel, double translation);
+
+    Point();
+    Point(double param1, double param2);
+    Point(const Point &newpoint);
+
+    double getOrdonnee() const;
+    void setOrdonnee(double ord);
+
+    double getAbscisse() const;
+    void setAbscisse(double abs);
+    Point& operator+=(const Point& valeur);
+
+};
+
+std::ostream& operator<<(std::ostream& os, Point const& p);
